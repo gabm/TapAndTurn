8bb8dd1e1ea88789bba9cb5e81023b67a8369b89
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2016-10-31 20:50:29 +0100

    Remove menu and fab

--- app/src/main/java/com/gabm/screenrotationcontrol/MainActivity.java
+++ app/src/main/java/com/gabm/screenrotationcontrol/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements Switch.OnCheckedC
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
